38317b875289114d92493902726ba94862c39a00
pranavjha <jha.pranav.s@gmail.com>
2014-09-24 18:27:43 +0530

    logger restructuring
    logger restructuring

--- test/index.js
+++ test/index.js
@@ -12,222 +12,29 @@ describe('base.logger', function () {
     });
     // it should not log anything when configured with an empty array
     it('should not log anything when configured with an empty array', function () {
-        logProvider.configure([]);
-        var origLogger = console.log, spyLogger = sinon.spy();
-        console.log = spyLogger;
-        var logger = logProvider('');
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
-        console.log = origLogger;
-        expect(spyLogger).to.have.callCount(0);
-    });
-    // it should respect patterns
-    it('should respect patterns (test 1)', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // first test
-        console.log = spyLogger;
-        logProvider.configure([{
-            pattern: '.*',
-            level: 'log'
-        }]);
-        var logger = logProvider('hello');
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
-        console.log = origLogger;
-        expect(spyLogger).to.have.callCount(4);
-    });
-    it('should respect patterns (test 2)', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // second test
-        console.log = spyLogger;
-        logProvider.configure([{
-            pattern: 'invalid',
-            level: 'log'
-        }]);
-        var logger = logProvider('hello');
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
-        console.log = origLogger;
-        expect(spyLogger).to.have.callCount(0);
-    });
-    it('should respect patterns (test 3)', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // third test
-        console.log = spyLogger;
-        logProvider.configure([{
-            pattern: /^[a-z]*$/,
-            level: 'log'
-        }]);
-        var logger = logProvider('hello');
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
-        logger = logProvider('hEllo');
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
-        console.log = origLogger;
-        expect(spyLogger).to.have.callCount(4);
-    });
-    // it should respect log levels
-    it('should respect log levels (test 1)', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // first test
-        console.log = spyLogger;
-        logProvider.configure([{
-            pattern: '.*',
-            level: 'error'
-        }]);
-        var logger = logProvider('hello');
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
-        console.log = origLogger;
-        expect(spyLogger).to.have.callCount(1);
-    });
-    it('should respect log levels (test 2)', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // second test
-        console.log = spyLogger;
-        logProvider.configure([{
-            pattern: '.*',
-            level: 'none'
-        }]);
-        var logger = logProvider('hello');
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
-        console.log = origLogger;
-        expect(spyLogger).to.have.callCount(0);
-    });
-    it('should respect log levels (test 2)', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // third test
-        spyLogger = sinon.spy();
-        console.log = spyLogger;
-        logProvider.configure([{
-            pattern: '.*',
-            level: 'info'
-        }]);
-        var logger = logProvider('hello');
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
-        logger = logProvider('hEllo');
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
-        console.log = origLogger;
-        expect(spyLogger).to.have.callCount(6);
-    });
-    // it should be able to log objects
-    it('should be able to log objects', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // first test
-        console.log = spyLogger;
-        logProvider.configure();
-        var logger = logProvider();
-        logger.log({
-            hello: 'world'
+        var logger = logProvider({
+            group: 'G1',
+            settings: []
         });
-        logger.info([1, 2, 3, 4]);
-        expect(spyLogger).to.have.callCount(2);
-        console.log = origLogger;
-    });
-    // should format newlines properly
-    it('should format newlines properly', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // first test
+        var origLogger = console.log;
+        var spyLogger = sinon.spy();
         console.log = spyLogger;
-        logProvider.configure();
-        var logger = logProvider();
-        logger.log('hello\nworld');
-        expect(spyLogger).to.have.callCount(1);
+        logger.debug(1).info(1).warn(1).error(1);
         console.log = origLogger;
-    });
-    // should log empty lines
-    it('should log empty lines', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // first test
-        console.log = spyLogger;
-        logProvider.configure();
-        var logger = logProvider();
-        logger.log();
-        expect(spyLogger).to.have.callCount(1);
-        console.log = origLogger;
-    });
-    // should not log with no-op appender
-    it('should not log with no-op appender', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // first test
-        console.log = spyLogger;
-        logProvider.configure([{
-            pattern: '.*',
-            appender: 'no-op'
-        }]);
-        var logger = logProvider();
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
         expect(spyLogger).to.have.callCount(0);
-        console.log = origLogger;
     });
-    // should not configure with invalid appenders
-    it('should fail silently invalid appenders', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // first test
+    // it should be able to configure properly with defaults
+    it('should be able to configure properly with defaults', function () {
+        var logger = logProvider({
+            group: 'G1'
+        });
+        var origLogger = console.log;
+        var spyLogger = sinon.spy(function () {
+            origLogger.apply(console, arguments);
+        });
         console.log = spyLogger;
-        logProvider.configure([{
-            pattern: '.*',
-            appender: 'invalid-appender'
-        }]);
-        var logger = logProvider();
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
-        expect(spyLogger).to.have.callCount(0);
+        logger.debug(1).info(1).warn(1).error(1);
         console.log = origLogger;
-    });
-    // should not configure with invalid appenders
-    it('should fail silently invalid levels', function () {
-        var origLogger = console.log,
-            spyLogger = sinon.spy();
-        // first test
-        console.log = spyLogger;
-        logProvider.configure([{
-            level: 'invalid log level'
-        }]);
-        var logger = logProvider();
-        logger.log(1);
-        logger.info(1);
-        logger.error(1);
-        logger.warn(1);
         expect(spyLogger).to.have.callCount(4);
-        console.log = origLogger;
     });
 });
